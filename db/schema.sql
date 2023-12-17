ee8400c4e1613b630021b1b37e868182f4bd060d
Luis Rubio <luisrubio7771@outlook.com>
2023-12-16 21:40:11 -0600

    created my database tables in the schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -4,4 +4,32 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
+CREATE TABLE department (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id),
+    REFERENCES department(id),
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id),
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id),
+    REFERENCES employee(id),
+    ON DELETE SET NULL
+)
+
 
