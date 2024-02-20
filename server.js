17568a227cda3c13e27d63777a50a5c85dfed017
Luis <luisrubio7771@outlook.com>
2024-02-20 13:20:56 -0600

    Added ability for user to add a department and role to the tracker

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Imports my necessary packages
 const express = require("express");
+const inquirer = require("inquirer")
 const mysql = require("mysql2");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -20,6 +21,103 @@ const db = mysql.createConnection(
     },
     console.log("Connected to the database")
 )
+
+const startMenu = () => {
+    inquirer.prompt([
+        //Begins command line by asking questions with inquirer
+        {
+        type: "list",
+        name: "prompt",
+        message: "what would you like to do?",
+        choices: ["View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role", "Log out"]
+        },
+    ])
+    .then((answers) => {
+        if (answers.prompt === "View all employees") {
+            db.query(`SELECT * FROM employee`, (err, result) => {
+                if (err) throw err;
+                console.log("Viewing all employees: ");
+                console.table(result);
+                startMenu();
+            });
+        }
+        else if (answers.prompt === "Add a department") {
+            inquirer.prompt([{
+                //Adding a department
+                type: "input", 
+                name: "department",
+                message: "What is the name of the department?",
+                validate: departmentInput => {
+                    if (departmentInput) {
+                        return true;
+                    } else {
+                        console.log("Add a department");
+                        return false;
+                    }
+                }
+            }]).then((answers) => {
+                db.query(`INSERT INTO department (name) VALUES (?)`, [answers.department], (err, result) => {
+                    if (err) throw err;
+                    console.log(`Added ${answers.department} to the database`)
+                    startMenu();
+                });
+            })
+        } else if (answers.prompt === "Add a role") {
+            db.query(`SELECT * FROM department`, (err, result) => {
+                if (err) throw err;
+
+                inquirer.prompt([
+                    {
+                        //Adding role
+                        type: "input",
+                        name: "role",
+                        message: "What is the name of the role?",
+                        validate: roleInput => {
+                            if (roleInput) {
+                                return true;
+                            } else {
+                                console.log("Please add a role");
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        //Adding the salary for the role
+                        type: "input",
+                        name: "salary",
+                        message: "what is the salary of the role?",
+                        validate: salaryInput => {
+                            if (salaryInput) {
+                                return true;
+                            } else {
+                                console.log("Pkease add a salary for the new role");
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        //Adding the department for the role
+                        type: "list",
+                        name: "department",
+                        message: "Which department does the role belong to?",
+                        choices: () => result.map(item => item.name)
+                    }
+                ]).then((answers) => {
+                    for (var i = 0; i < result.length; i++) {
+                        if (result[i].name === answers.department) {
+                            var department = result[i];
+                        }
+                    }
+                    db.query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`, [answers.role, answers.salary, department.id], (err, result) => {
+                        if (err) throw err;
+                        console.log(`Added ${answers.role} to the database`)
+                        startMenu();
+                    });
+                })
+            });
+        } else if
+    })
+}
 
 
 
